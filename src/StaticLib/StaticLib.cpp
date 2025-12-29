c501d1400ba841f0a5b3d44beefa5b8f4cfd49a6
g2527056-del <g2527056@st.t-kougei.ac.jp>
2025-12-29 15:02:01 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,24 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin == NULL || end == NULL || begin >= end) return false;
+	size_t n = end - begin;
+	bool swapped = false;
+	for (size_t i = 0; i < n-1; i++)
+	{
+		for (size_t j = 0; j < n - 1 - i; j++)
+		{
+			// 隣同士を比較
+			if (begin[j].key > begin[j + 1].key)
+			{
+				item tmp = begin[j];
+				begin[j] = begin[j + 1];
+				begin[j + 1] = tmp;
+				swapped = true;
+			}
+		}
+		if (!swapped) break;
+	}
+
+	return true;
 }
